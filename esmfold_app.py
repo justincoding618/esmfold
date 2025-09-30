adecc483590ce9bf6d49f7a63ecae6138cda6a67
justincoding618 <justinhsu618@gmail.com>
2025-09-30 01:54:48 -0400

    define molecule render function

--- esmfold_app.py
+++ esmfold_app.py
@@ -6,3 +6,14 @@ import biotite.structure.io as bsio
 
 st.sidebar.title('ESMFold')
 st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model.  You can learn more from the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
+
+# set up stmol
+def render_moleculr(pdb):
+	pdbview = py3Dmol.view()
+	pdbview.addModel(pdb,'pdb')
+	pdbview.setStyle({'cartoon':{'color':"spectrum"}})
+	pdbview.setBackgroundColor('black')
+	pdbview.zoomTo()
+	pdbview.zoom(2, 800)
+	pdbview.spin(True)
+	showmol(pdbview, height = 500, width = 800)
