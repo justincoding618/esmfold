c292aa18c123d33d403896da533a5c9381d1503a
justincoding618 <justinhsu618@gmail.com>
2025-09-30 01:55:41 -0400

    fix molecule render function name

--- esmfold_app.py
+++ esmfold_app.py
@@ -8,7 +8,7 @@ st.sidebar.title('ESMFold')
 st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model.  You can learn more from the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
 # set up stmol
-def render_moleculr(pdb):
+def render_molecule(pdb):
 	pdbview = py3Dmol.view()
 	pdbview.addModel(pdb,'pdb')
 	pdbview.setStyle({'cartoon':{'color':"spectrum"}})
