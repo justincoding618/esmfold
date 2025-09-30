b4dc8cd1dc3f49dd7348c54bc829ed0af73f65af
justincoding618 <justinhsu618@gmail.com>
2025-09-30 01:50:58 -0400

    set up application sidebar

--- esmfold_app.py
+++ esmfold_app.py
@@ -3,3 +3,6 @@ from stmol import showmol
 import py3Dmol
 import requests
 import biotite.structure.io as bsio
+
+st.sidebar.title('ESMFold')
+st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model.  You can learn more from the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
