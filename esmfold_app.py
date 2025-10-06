70c919eacf423a411f98088ad1e886f5920945d6
justincoding618 <justinhsu618@gmail.com>
2025-10-06 19:42:44 -0400

    add functionality to update function

--- esmfold_app.py
+++ esmfold_app.py
@@ -24,4 +24,37 @@ txt = st.sidebar.text_area('Input Sequence', DEFAULT_SEQ, height=275)
 
 # The ESMFold Application
 def update(sequence=txt):
-	
+	headers = {
+	'Content-Type': 'application/w-www-form-urlencoded'
+	}
+	response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb', headers=headers, data=sequence)
+	name = sequence[:3] + sequence[-3:]
+	pdb_string = response.content.decode('utf-8')
+
+	with open('predicted.pdb', 'w') as function:
+		function.write(pdb_string)
+
+	structure = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
+	b_value = round(structure.b_factor.mean(), 4)
+
+	#Display Protein Structure
+	st.subheader('Visualization of Predicted Protein Structure')
+	render_molecule(pdb_string)
+
+	#Get p1DDT Values; p1DDT is stored in B-factor field
+	st.subheader('p1DDT')
+	st.write('p1DDT is a per-residue of the confidence in prediction on a scale from 1-100.')
+	st.info(f'p1DDT: {b_value}')
+
+	st.download_button(
+		label="Download PDB",
+		data=pdb_string,
+		file_name='predicted_pdb',
+		mime='text/plain'
+		)
+
+predict = st.sidebar.button('Predict', on_click=update)
+
+if not predict:
+	st.warning('Please enter your protein sequence data!')
+
